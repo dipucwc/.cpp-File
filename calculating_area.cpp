cec82ff9083f12826ec5a3e8b502ff186b802f59
Md Moklesur Rahman <42492937+dipucwc@users.noreply.github.com>
2021-03-21 16:41:51 +0200

    Add files via upload
    Basic C++ programming

---
+++ calculating_area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//using namespace std;
+
+#define LENGTH 13.5
+#define WIDTH  6.5
+#define NEWLINE '\n'
+
+int main()
+{
+   float area;
+
+   area = LENGTH * WIDTH;
+   std::cout << area;
+   //cout << NEWLINE;
+   return 0;
+}
